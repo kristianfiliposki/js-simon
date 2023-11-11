2518d520a4627a50e9691d7ab5816e99b9ea5cde
kristiafiliposki <filiposki.kristian1112@gmail.com>
2023-11-11 15:16:04 +0100

    1.0

--- java/main.js
+++ java/main.js
@@ -1,37 +1,46 @@
 
 let container=document.getElementById("container");
 let numeriCasuali=[];
-let dati=44;
+let numeriInseriti=[];
+let numero1="";
+let numero2="";
+let numero3="";
+let numero4="";
+let numero5="";
 
-setTimeout(genNumb(numeriCasuali),5*1000)
-
-function genNumb(arrey){
+function genNumb(arrey,elemento){
     for (let i = 0; i < 5; i++) {
         let numero=arrey[i]
         numero=(Math.floor(Math.random() * 100));
         arrey.push(numero);
-        
-        
     }
-    console.log(arrey);
+    let risultato=(elemento.innerHTML+=arrey);
+    return risultato;
 }
 
-/*  scrivo i numeri generati */ 
-/*  container.innerHTML=(genNumb(numeriCasuali))
- */
+genNumb(numeriCasuali,container);
 
-/* setTimeout(myfunction,4000);
-
- function myfunction(){
-    console.log(`cioa mamma`)
-}
- */
+setTimeout(function (){
+    container.classList.add("hidden");  
+},1000*2)
 
 
+setTimeout(function (){
+    let i = 0
+    while (i < 5){
+        let numero = numeriInseriti[i];
+        numero=Number(prompt("scrivi il numero visualizzato"));
+        numeriInseriti.push(numero);
+        i++;}
+    for (let i = 0; i < numeriInseriti.length; i++) {
+        let numero = numeriInseriti[i];
+            if(numeriCasuali.includes(numero)){
+                console.log("è corretto")
+            }
+            else if(!(numeriCasuali.includes(numero))){
+                alert("hai perso!")
+            }
+        }
+    console.log(numeriInseriti);
+},1000*2.3);
 
-/* creon una funzione di numeri */
-function reading(numeri){
-    console.log(numeri)
-}
-/* tali dati appaiono dopo 3s */
-setTimeout(reading(dati),3000);
